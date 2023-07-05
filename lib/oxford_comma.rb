c8c6b6486197ebf22708df0495af0aa6471c981f
Kennedy Musau <kennedy.musauu@gmail.com>
2023-07-05 18:41:16 +0300

    complete

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  case array.size
+  when 0
+    ""
+  when 1
+    array[0]
+  when 2
+    array.join(" and ")
+  else
+    last_element = ", and #{array.last}"
+    array[0..-2].join(", ") + last_element
+  end
 end
